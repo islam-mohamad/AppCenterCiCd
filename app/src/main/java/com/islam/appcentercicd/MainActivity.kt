ed0722bdcd42246e14f7a52a9e03bf6af2c8af65
islam-mohamad <islam.salaawy@gmail.com>
2022-11-18 11:03:50 +0200

    fix: compilation error

--- app/src/main/java/com/islam/appcentercicd/MainActivity.kt
+++ app/src/main/java/com/islam/appcentercicd/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.islam.appcentercicd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCenter.start(getApplication(), "78a15f84-5697-4886-88c8-cd9cc3bcfaf2",
-                  Analytics.class, Crashes.class);
+        AppCenter.start(
+            application, "78a15f84-5697-4886-88c8-cd9cc3bcfaf2",
+            Analytics::class.java, Crashes::class.java
+        );
     }
 }
