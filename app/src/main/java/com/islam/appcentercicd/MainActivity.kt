01db467612451bf880ed05ba5fe6468cfcb6960c
Islam Mohamed <islam.salaawy@gmail.com>
2022-11-18 09:46:53 +0200

    Update MainActivity.kt
    add app center analytics

--- app/src/main/java/com/islam/appcentercicd/MainActivity.kt
+++ app/src/main/java/com/islam/appcentercicd/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(getApplication(), "78a15f84-5697-4886-88c8-cd9cc3bcfaf2",
+                  Analytics.class, Crashes.class);
     }
 }
